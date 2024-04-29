f868c001a05fb4a266189a9cf7827326c620aa9b
xiayoushuang <xiayoushuang@sfere-elec.com>
2024-04-29 10:27:27 +0800

    add example

---
+++ example/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/Kress4s/kslog"
+
+func main() {
+	// log setting
+	kl := kslog.New("your app name")
+	kl.SetFormatter(&kslog.PrettyJSONFormatter)
+	kl.SetLevel(kslog.TranceLevel)
+
+	// log
+	kl.Debugf("test log, app: %s", "test")
+	kl.Traceln("test")
+	// ...
+}
